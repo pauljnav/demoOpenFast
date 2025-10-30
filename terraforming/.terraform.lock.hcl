42933cfc59cfd44f7f884b8bbd12c5abd82c3900
pauljnav <tecknikp@gmail.com>
2025-10-30 02:09:56 +0000

    insert SKU Standard_D2s_v3

--- terraforming/.terraform.lock.hcl
+++ terraforming/.terraform.lock.hcl
@@ -5,6 +5,7 @@ provider "registry.terraform.io/hashicorp/azurerm" {
   version     = "3.117.1"
   constraints = "~> 3.0"
   hashes = [
+    "h1:39rusALk6qOtHhHQ18sy0Fgnv/0g9Iuqtw8KXk+WZPM=",
     "h1:Y9J+emydHX7LpJYKdF9Zmng0zcRR86Awm/t28LNzb68=",
     "zh:0c513676836e3c50d004ece7d2624a8aff6faac14b833b96feeac2e4bc2c1c12",
     "zh:50ea01ada95bae2f187db9e926e463f45d860767a85ebc59160414e00e76c35d",
--- terraforming/main.tf
+++ terraforming/main.tf
@@ -1,12 +1,15 @@
 # Variables
-variable "resource_group_name" { default = "rg-6core-openfast" }
-variable "location" { default = "East US" }
+variable "resource_group_name" { default = "rg-D2s2v8r-openfast" }
+variable "location" { default = "westeurope" }
 variable "vm_name" { default = "openfast-vm" }
+# Ensure your SSH public key is available
+variable "public_key_path" { default = "~/.ssh/id_rsa.pub" }
+variable "ssh_username" { default = "azurepauljn" }
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "rg-6core-openfast"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 # Create a virtual network within the resource group
@@ -24,10 +27,95 @@ resource "azurerm_subnet" "main" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# Create Public IP
 resource "azurerm_public_ip" "example" {
   name                = "pip-${var.vm_name}"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
   sku                 = "Standard"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "example" {
+  name                = "nsg-${var.vm_name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "example" {
+  name                = "nic-${var.vm_name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+# VM Configuration
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = var.vm_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_D2s_v3" # 2 vCPUs, 4 GB RAM - much smaller and cost-effective
+  admin_username      = "azurepauljn"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+#  Other good small VM size options you could consider:
+# Standard_B1s - 1 vCPU, 1 GB RAM (smallest/cheapest)
+# Standard_B1ms - 1 vCPU, 2 GB RAM
+# Standard_B2s - 2 vCPUs, 4 GB RAM
+# Standard_D2s_v3 - 2 vCPUs, 8 GB RAM (current choice)
+
+  # Installation script for Conda/OpenFAST
+  custom_data = filebase64("${path.module}/install_conda_openfast.sh")
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  computer_name = var.vm_name
+
+  # SSH Key Authentication
+  admin_ssh_key {
+    username   = var.ssh_username
+    public_key = file(var.public_key_path)
+  }
+}
+
+# Output Public IP for SSH
+output "public_ip_address" {
+  value = azurerm_public_ip.example.ip_address
 }
